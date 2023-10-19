b123138b55f6b10f13291ee249d1113e3fe44950
HP <parthpatel18122@gmail.com>
2023-10-19 08:05:17 +0530

    p9

--- app/src/main/java/com/example/mad_parctical9_095/MainActivity.kt
+++ app/src/main/java/com/example/mad_parctical9_095/MainActivity.kt
@@ -8,22 +8,33 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     lateinit var alarmAnimation : AnimationDrawable
+    lateinit var heartAnimation : AnimationDrawable
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val alarm :ImageView = findViewById(R.id.image)
+        val alarm :ImageView = findViewById(R.id.alarm)
         alarm.setBackgroundResource(R.drawable.alarm_animation_list)
         alarmAnimation=alarm.background as AnimationDrawable
+
+        val heart :ImageView = findViewById(R.id.heart)
+        alarm.setBackgroundResource(R.drawable.heart_animation_list)
+        heartAnimation=heart.background as AnimationDrawable
+
+
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if(hasFocus){
             alarmAnimation.start()
+            heartAnimation.start()
+
         }
         else{
             alarmAnimation.stop()
+            heartAnimation.stop()
         }
     }
 }
